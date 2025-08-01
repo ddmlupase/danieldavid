10ce39ab4663ec440a138c92514c8d54c3c75930
Daniel David <146162731+ddmlupase@users.noreply.github.com>
2025-08-01 22:47:44 +0800

    feat: update page with new certifications and images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Navbar from "@/components/navbar";
 import Image from 'next/image';
 import me from '@/assets/profile.jpg';
+import introsql from '@/assets/introsql.jpg';
+import sqlintermediate from '@/assets/sqlintermediate.jpg';
 
 export default function Home() {
   return (
@@ -169,7 +171,7 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="flex flex-col md:flex-row gap-5 mt-20">
+          <div className="flex flex-col md:flex-row gap-5 mt-10">
 
             <div className="md:w-[33%]">
 
@@ -215,6 +217,113 @@ export default function Home() {
 
         </div>
       </div>
+      {/* ✅ status */}
+      <div className="max-w-6xl mx-auto">
+
+        <div className="flex flex-col items-center justify-center mt-50 mb-50">
+        <h1 className="text-6xl font-bold font-reddit-sans text-[#222222]">
+          DANIEL DAVID SO FAR HAS
+        </h1>
+        
+        <ul className="flex text-[#222222] font-reddit-sans mt-5 gap-10 max-w-1xl">
+          <li className="flex items-center">
+            <h1 className="font-bold text-6xl mr-2 font-reddit-sans">01</h1>
+            <p>Clients.</p>
+          </li>
+
+        <li className="flex items-center">
+        <h1 className="font-bold text-6xl mr-2">03</h1>
+        <p>Years of Experience.</p>
+        </li>
+
+        <li className="flex items-center">
+        <h1 className="font-bold text-6xl mr-2">02</h1>
+        <p>Obtained Certifications.</p>
+        </li>
+
+        <li className="flex items-center">
+        <h1 className="font-bold text-6xl mr-2">04</h1>
+        <p>Projects Made.</p>
+        </li>
+        </ul>
+
+        </div>
+        </div>
+
+        {/** Certifications Section */}
+        <div className="w-full bg-[#222222] py-24">
+
+
+          <div className="max-w-6xl mx-auto flex flex-col md:flex-row gap-10">
+
+            <div className="text-white md:w-[32%]">
+              <h3>Check Out</h3>
+              <h1 className="text-4xl font-bold mb-7 mt-1">MY CERTIFICATIONS</h1>
+              <p className="">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+              <button className="mt-10 text-white text-sm bg-transparent border border-white px-3 py-2 rounded-md hover:bg-white hover:text-black cursor-pointer">
+              More Certifications
+              </button>
+
+            </div>
+
+            <div className="text-white md:w-[66%]">
+
+              <ul className="flex flex-col md:flex-row justify-between font-reddit-sans">
+
+                <li>
+                  <div className="relative bg-white md:w-[245px] h-[150px] overflow-hidden mb-2">
+                    <Image
+                      src={sqlintermediate}
+                      alt="me"
+                      fill
+                      className="object-cover" // or use object-contain
+                    />
+                  </div>
+  
+                  <p className="text-center font-semibold">Intermediate SQL</p>
+                  <p className="text-center font-thin">Sololearn</p>
+                </li>
+
+                <li>
+                  <div className="bg-white md:w-[245px] h-[150px] text-black flex items-center justify-center mb-2">
+                    Coming Soon!
+                  </div>
+                  <p className="text-center font-semibold">Introduction to Networks</p>
+                  <p className="text-center font-thin">Cisco</p>
+                </li>
+
+                <li>
+                  <div className="relative bg-white md:w-[245px] h-[150px] overflow-hidden mb-2">
+                    <Image
+                      src={introsql}
+                      alt="me"
+                      fill
+                      className="object-cover" // or use object-contain
+                    />
+                  </div>
+                  <p className="text-center font-semibold">Introduction to SQL</p>
+                  <p className="text-center font-thin">Sololearn</p>
+                </li>
+              </ul>
+
+            </div>
+          </div>
+        </div>
+
+        {/* ✅ Footer Section */}
+        <div className="max-w-6xl mx-auto">
+
+          <div className="flex flex-col items-center justify-center mt-40 mb-40">
+
+            <h1 className="text-6xl font-bold font-reddit-sans text-[#222222]">
+              MY TECH STACK
+            </h1>
+            <p className="text-lg font-reddit-sans text-[#222222]">Technologies I have been working with recently</p>
+
+          </div>
+
+        </div>
     </>
+    
   );
 }
