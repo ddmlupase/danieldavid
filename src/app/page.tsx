8881a3d4524eb7b9b82b0024f51adadeb00b78f6
Daniel David <146162731+ddmlupase@users.noreply.github.com>
2025-08-02 16:17:03 +0800

    feat: Update page layout and add new assets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,22 @@ import Image from 'next/image';
 import me from '@/assets/profile.jpg';
 import introsql from '@/assets/introsql.jpg';
 import sqlintermediate from '@/assets/sqlintermediate.jpg';
+import database from '@/assets/database.png';
+import domain from '@/assets/domain.png';
+import window from '@/assets/window.png';
+import mysql from '@/assets/mysql.png';
+import js from '@/assets/js.png';
+import html from '@/assets/html.png';
+import css from '@/assets/css.png';
+import ts from '@/assets/ts.png';
+import nextjs from '@/assets/nextjs.png';
+import visualstudio from '@/assets/visualstudio.png';
+import github from '@/assets/github.png';
+import vscode from '@/assets/vscode.png';
+import drawio from '@/assets/drawio.png';
+import nodejs from '@/assets/nodejs.png';
+import prisma from '@/assets/prisma.png';
+
 
 export default function Home() {
   return (
@@ -56,30 +72,45 @@ export default function Home() {
           <div className="flex justify-between items-center w-full">
             <h2 className="text-white text-5xl font-bold">MY SERVICES</h2>
             <button className="text-white text-sm bg-transparent border border-white px-3 py-2 rounded-md hover:bg-white hover:text-black cursor-pointer">
-            See More
+            More Services
             </button>
             </div>
 
-          <div className="flex font-reddit-sans justify-between gap-55 mt-15">
-            <div>
-              <h3 className="text-white text-2xl font-medium mt-10">DATABASE DESIGN & MODELING</h3>
-              <p className="text-white text-base font-light mt-10">
-                Crafting efficient and scalable database architectures tailored to application needs.
-                This is a sentence.
+          <div className="flex font-reddit-sans justify-between gap-50 mt-15 md:w-[100%]">
+
+            <div className="md:w-[33%]">
+              <Image
+                src={database}
+                alt="database"
+                width={50}
+              />
+              <h3 className="text-white text-2xl font-medium mt-8">RELATIONAL DATABASE DESIGN SERVICE</h3>
+              <p className="text-white text-base font-thin mt-9">
+                Designing ER diagrams and building structured database schemas using MySQL Workbench.
               </p>
             </div>
-            <div>
-              <h3 className="text-white text-2xl font-medium mt-10">DATABASE DEVELOPMENT</h3>
-              <p className="text-white text-base font-light mt-10">
-                Crafting efficient and scalable database architectures tailored to application needs.
-                This is a sentence.
+
+            <div className="md:w-[33%]">
+              <Image
+                src={domain}
+                alt="domain"
+                width={50}
+              />
+              <h3 className="text-white text-2xl font-medium mt-8">WEBSITE DEVELOPMENT</h3>
+              <p className="text-white text-base font-thin mt-17">
+                Developing responsive websites with both frontend and backend functionality.
               </p>
             </div>
-            <div>
-              <h3 className="text-white text-2xl font-medium mt-10">DBA SUPPORT</h3>
-              <p className="text-white text-base font-light mt-18">
-                Crafting efficient and scalable database architectures tailored to application needs.
-                This is a sentence.
+
+            <div className="md:w-[33%]">
+              <Image
+                src={window}
+                alt="window"
+                width={50}
+              />
+              <h3 className="text-white text-2xl font-medium mt-8">WINDOWS SOFTWARE DEVELOPMENT</h3>
+              <p className="text-white text-base font-thin mt-8">
+                Creating custom Windows applications using C# and Visual Studio.
               </p>
             </div>
 
@@ -97,13 +128,13 @@ export default function Home() {
           <div className="flex justify-between items-center w-full mt-40">
             <h2 className="text-[#222222] text-5xl font-bold">MY FEATURED PROJECTS</h2>
             <button className="mt-8 bg-white text-[#222222] hover:bg-[#222222] hover:text-white border border-[#222222] px-4 py-1 rounded-md cursor-pointer">
-            See More
+            More Projects
             </button>
           </div>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          <p>These featured projects highlight my experience in web development, database design, and application building.</p>
 
           
-          <div className="flex flex-col md:flex-row gap-5 mt-20">
+          <div className="flex flex-col md:flex-row gap-5 mt-14">
 
             <div className="md:w-[33%]">
 
@@ -151,7 +182,7 @@ export default function Home() {
             <div className="md:w-[33%]">
 
               <div className="bg-[#222222] md:w-[100%] h-[200px] flex justify-center items-center rounded-md">
-                <p className="text-white">Project 3 Image</p>
+                <p className="text-white">Coming Soon...</p>
               </div>
               
               <p className="text-[#222222] text-1xl font-thin mt-2 font-reddit-sans">Entity Relationship Diagram</p>
@@ -176,7 +207,7 @@ export default function Home() {
             <div className="md:w-[33%]">
 
               <div className="bg-[#222222] md:w-[100%] h-[200px] flex justify-center items-center rounded-md">
-                <p className="text-white">Project 4 Image</p>
+                <p className="text-white">Coming Soon...</p>
               </div>
 
               <p className="text-[#222222] text-1xl font-thin mt-2 font-reddit-sans">Entity Relationship Diagram</p>
@@ -196,7 +227,7 @@ export default function Home() {
             <div className="md:w-[33%]">
 
               <div className="bg-[#222222] md:w-[100%]  h-[200px] flex justify-center items-center rounded-md">
-                <p className="text-white">Project 5 Image</p>
+                <p className="text-white">Coming Soon...</p>
               </div>
 
               <p className="text-[#222222] text-1xl font-thin mt-2 font-reddit-sans ">Entity Relationship Diagram</p>
@@ -225,26 +256,26 @@ export default function Home() {
           DANIEL DAVID SO FAR HAS
         </h1>
         
-        <ul className="flex text-[#222222] font-reddit-sans mt-5 gap-10 max-w-1xl">
+        <ul className="flex text-[#222222] font-reddit-sans mt-5 gap-8 max-w-1xl">
           <li className="flex items-center">
-            <h1 className="font-bold text-6xl mr-2 font-reddit-sans">01</h1>
-            <p>Clients.</p>
+            <h1 className="font-bold text-8xl">01</h1>
+            <p className="text-lg">Clients.</p>
           </li>
 
-        <li className="flex items-center">
-        <h1 className="font-bold text-6xl mr-2">03</h1>
-        <p>Years of Experience.</p>
-        </li>
+          <li className="flex items-center">
+            <h1 className="font-bold text-8xl mr-2">03</h1>
+            <p className="text-lg">Years of Experience.</p>
+          </li>
 
-        <li className="flex items-center">
-        <h1 className="font-bold text-6xl mr-2">02</h1>
-        <p>Obtained Certifications.</p>
-        </li>
+          <li className="flex items-center">
+            <h1 className="font-bold text-8xl mr-2">02</h1>
+            <p className="text-lg">Obtained Certifications.</p>
+          </li>
 
-        <li className="flex items-center">
-        <h1 className="font-bold text-6xl mr-2">04</h1>
-        <p>Projects Made.</p>
-        </li>
+          <li className="flex items-center">
+            <h1 className="font-bold text-8xl mr-2">04</h1>
+            <p className="text-lg">Projects Made.</p>
+          </li>
         </ul>
 
         </div>
@@ -259,7 +290,7 @@ export default function Home() {
             <div className="text-white md:w-[32%]">
               <h3>Check Out</h3>
               <h1 className="text-4xl font-bold mb-7 mt-1">MY CERTIFICATIONS</h1>
-              <p className="">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+              <p className="">A collection of certifications I’ve earned to support my learning and development in tech.</p>
               <button className="mt-10 text-white text-sm bg-transparent border border-white px-3 py-2 rounded-md hover:bg-white hover:text-black cursor-pointer">
               More Certifications
               </button>
@@ -310,16 +341,98 @@ export default function Home() {
           </div>
         </div>
 
-        {/* ✅ Footer Section */}
+        {/* ✅ tech stack */}
         <div className="max-w-6xl mx-auto">
 
-          <div className="flex flex-col items-center justify-center mt-40 mb-40">
+          <div className="flex flex-col items-center justify-center mt-40 mb-15">
 
-            <h1 className="text-6xl font-bold font-reddit-sans text-[#222222]">
+            <h1 className="text-6xl font-bold font-reddit-sans text-[#222222] mb-3">
               MY TECH STACK
             </h1>
-            <p className="text-lg font-reddit-sans text-[#222222]">Technologies I have been working with recently</p>
+            <p className="text-lg font-reddit-sans text-[#222222]">Here are the tools and technologies I use in my recent projects</p>
 
+          </div>
+
+          <div className="mb-50">
+
+            <ul className="flex gap-25 text-222222 font-thin text-sm sm:text-base font-reddit-sans justify-center mb-25">
+            <li className="flex justify-center items-center"><Image
+                src={mysql}
+                alt="mysql"
+                width={100}
+              /></li>
+            <li className="flex justify-center items-center"><Image
+                src={js}
+                alt="js"
+                width={100}
+              /></li>
+            <li className="flex justify-center items-center"><Image
+                src={css}
+                alt="css"
+                width={100}
+              /></li>
+            <li className="flex justify-center items-center"><Image
+                src={html}
+                alt="html"
+                width={100}
+              /></li>
+            <li className="flex justify-center items-center"><Image
+                src={ts}
+                alt="ts"
+                width={100}
+              /></li>
+              <li className="flex justify-center items-center"><Image
+                src={vscode}
+                alt="vscode"
+                width={100}
+              /></li>
+            </ul>
+
+            <ul className="flex gap-25 text-222222 font-thin text-sm sm:text-base font-reddit-sans justify-center mb-20">
+            <li className="flex justify-center items-center"><Image
+                src={nextjs}
+                alt="nextjs"
+                width={100}
+              /></li>
+            <li className="flex justify-center items-center"><Image
+                src={drawio}
+                alt="drawio"
+                width={100}
+              /></li>
+            <li className="flex justify-center items-center"><Image
+                src={github}
+                alt="github"
+                width={100}
+              /></li>
+            <li className="flex justify-center items-center"><Image
+                src={visualstudio}
+                alt="visualstudio"
+                width={100}
+              /></li>
+            <li className="flex justify-center items-center"><Image
+                src={nodejs}
+                alt="nodejs"
+                width={100}
+              /></li>
+              <li className="flex justify-center items-center"><Image
+                src={prisma}
+                alt="prisma"
+                width={100}
+              /></li>
+            </ul>
+
+          </div>
+
+        </div>
+
+        <div className="w-full bg-[#222222] py-5">
+
+          <div className="flex flex-col items-center justify-center">
+            <h1 className="text-white">hello</h1>
+          </div>
+
+          <div className="flex flex-col items-center justify-center">
+            <h1 className="text-white font-bold">© Daniel David Lupase 2025</h1>
           </div>
 
         </div>
